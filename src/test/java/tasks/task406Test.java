170eb81db7ac5311f6fe23ad73ecf01fd4303bf4
alexalexup <upvector.by@gmail.com>
2021-12-03 02:09:31 +0300

    Edit tests

--- src/test/java/tasks/task406Test.java
+++ src/test/java/tasks/task406Test.java
@@ -8,13 +8,13 @@ import static org.junit.jupiter.api.Assertions.*;
 class task406Test {
 
     @Test
-    void findSecondMaxWhenMaxInFirst() {
+     public void findSecondMaxWhenMaxInFirst() {
         int[] a = new int[]{2, 10, 1};
         Assertions.assertEquals(2, task406.findSecondMax(a));
     }
 
     @Test
-    void findSecondMaxWhenMaxInLast() {
+    public void findSecondMaxWhenMaxInLast() {
         int[] a = new int[]{5, 2, 4};
         Assertions.assertEquals(4, task406.findSecondMax(a));
         Assertions.assertArrayEquals(new int[]{5, 2, 4}, a);
@@ -22,14 +22,14 @@ class task406Test {
     }
 
     @Test
-    void findSecondMaxWhenMaxInMiddle() {
+    public void findSecondMaxWhenMaxInMiddle() {
         int[] a = new int[]{8, 7, 2};
         Assertions.assertEquals(7, task406.findSecondMax(a));
         Assertions.assertArrayEquals(new int[]{8, 7, 2}, a);
     }
 
     @Test
-    void findSecondMaxWhenArrayContainsFewSecondMaxElements() {
+    public void findSecondMaxWhenArrayContainsFewSecondMaxElements() {
         int[] a = new int[]{6, 0, 7, 8, 7};
         Assertions.assertEquals(7, task406.findSecondMax(a));
         Assertions.assertArrayEquals(new int[]{6, 0, 7, 8, 7}, a);
@@ -37,10 +37,14 @@ class task406Test {
     }
 
     @Test
-    void findSecondMaxWhenArrayContainsSingleElements() {
+    public void findSecondMaxWhenArrayContainsSingleElements() {
         int[] a = new int[]{-5};
         Assertions.assertEquals(-5, task406.findSecondMax(a));
     }
+
+
+
+
 
 
 }
