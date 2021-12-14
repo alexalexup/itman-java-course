2f4a7a148fc0979366d34cc748fd08d01b50fe5a
alexalexup <upvector.by@gmail.com>
2021-12-14 23:08:00 +0300

    Add  function calcPrimeNumbers for Task408

---
+++ src/main/java/tasks/Task432.java
@@ -0,0 +1,59 @@
+package tasks;
+
+public class Task432 {
+    public static long[] calcPrimeNumbers( long x) {
+        long b = x;
+        long p =x;
+        int max = 0;
+        int max2 = 0;
+        int j = 0;
+        if (x < 2) {
+            long[] actual = new long[] {1};
+            return actual;
+        }
+        for (int i = 2; i < (long)Math.sqrt(b) + 1; i++) {
+            if (b % i == 0) {
+                b = b/ i;
+                if (i > max) {
+                    max =i;
+                    j++;
+                }
+                i = 1;
+            }
+        }
+        if (max >= b) {
+            int index = 0;
+            long[] actual = new long[j];
+            for (int i =2; i < (long)Math.sqrt(b) + 1; i++ ) {
+                if (x % i == 0) {
+                    x = x / i;
+                    if (i > max2 && index < j) {
+                        max2 = i;
+                        actual[index] = i;
+                        index++;
+                    }
+                    i = 1;
+                }
+            }
+            return actual;
+        }
+        if (max < b) {
+            int index = 0;
+            long[] actual = new long[j+1];
+            for (int i =2; i < (long)Math.sqrt(b) + 1; i++ ) {
+                if (x % i == 0) {
+                    x = x / i;
+                    if (i > max2 && index < j+1) {
+                        max2 = i;
+                        actual[index] = i;
+                        index++;
+                    }
+                    i = 1;
+                }
+            }
+            actual[j] =b;
+            return actual;
+        }
+        return null;
+    }
+}
---
+++ src/test/java/tasks/Task432Test.java
@@ -0,0 +1,19 @@
+package tasks;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Task432Test {
+    @Test
+    public void FirstTest () {
+        long a = 999999999999998L;
+        long[] b = Task432.calcPrimeNumbers(a);
+        for (int i =0; i < b.length; i++) {
+            System.out.println("!!!!!  " + b[i]);
+        }
+
+
+    }
+
+}
