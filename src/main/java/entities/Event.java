0c188005f7706d83a37d551430bc622ee8286507
alexalexup <upvector.by@gmail.com>
2022-01-02 15:53:49 +0300

    Add toString function and test for Event class

--- src/main/java/entities/Event.java
+++ src/main/java/entities/Event.java
@@ -6,10 +6,29 @@ public class Event {
     public int day;
     public String name;
 
+    public Event () {
+
+    }
+
     public Event (int year, int month, int day, String name) {
         this.year = year;
         this.month = month;
         this.day = day;
         this.name = name;
+   }
+
+    public String toString() {
+        String year = "year=" + this.year;
+        String month= ", month=" + this.month;
+        String day = ", day=" + this.day;
+        String name = ", name=" + this.name;
+        if (this.year == 0 || this.month == 0 || this.day == 0 || this.name == null) {
+            String info = "";
+            return info;
+        }
+        String info = "Event{" + year + month + day + name +"}";
+        return info;
     }
 }
+
+
---
+++ src/test/java/entities/EventTest.java
@@ -0,0 +1,37 @@
+package entities;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EventTest {
+
+    @Test
+    public void toStringShouldReturnEqualStringArgumentThatIsEvent() {
+        Event event = new Event(2028,8, 24, "Birthday");
+        String expected = event.toString();
+        String actual = event + "";
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void toStringShouldNotChangeEventWhenMethodWasCalled() {
+        Event event = new Event(2028,8, 24, "Birthday");
+        Event expected = event;
+        Event expectedValues = new Event(event.year, event.month, event.day, event.name);
+        event.toString();
+        Assertions.assertSame(expected, event);
+        Assertions.assertEquals(expectedValues.year, event.year);
+        Assertions.assertEquals(expectedValues.month, event.month);
+        Assertions.assertEquals(expectedValues.day, event.day);
+        Assertions.assertEquals(expectedValues.name, event.name);
+    }
+
+    @Test
+    public void toStringShouldNotReturnDataWhenEventHaveNotData() {
+        Event event = new Event();
+        String inform = event.toString();
+        Assertions.assertEquals(inform, "");
+    }
+}
