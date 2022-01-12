f55828b837b2e21188f61411454512d622cb716c
alexalexup <upvector.by@gmail.com>
2022-01-13 01:29:36 +0300

    Create MyString class and methods, add tests

---
+++ src/main/java/tasks/MyString.java
@@ -0,0 +1,32 @@
+package tasks;
+
+public class MyString {
+    private char[] smb;
+
+    public MyString(char[] smb) {
+        this.smb = smb;
+    }
+
+    public String toString() {
+        if ( smb == null) {
+            return null;
+        }
+        String result = new String(smb);
+        return result;
+    }
+
+    public int length() {
+        if ( smb == null) {
+            int result = 0;
+            return result;
+        }
+        int result = smb.length;
+        return result;
+    }
+
+    public char charArt(int index) {
+        char result = smb[index];
+        return result;
+    }
+
+}
---
+++ src/test/java/tasks/MyStringTest.java
@@ -0,0 +1,75 @@
+package tasks;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MyStringTest {
+
+    @Test
+    public void toStringShouldReturnEmptyStringWhenFieldSmbHaveNotSymbols() {
+        MyString a = new MyString(new char[]{});
+        String expectedResult = "";
+        String actualResult = a.toString();
+        Assertions.assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void toStringShouldReturnResultWhenFieldSmbHaveOneSymbol() {
+        MyString a = new MyString(new char[]{'h'});
+        String expectedResult = "h";
+        String actualResult = a.toString();
+        Assertions.assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void toStringShouldReturnNullWhenFieldSmbIsNull() {
+        MyString a = new MyString(null);
+        String expectedResult = null;
+        String actualResult = a.toString();
+        Assertions.assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void toStringShouldReturnResultWhenFieldSmbHaveCharAndDecInOneArray() {
+        MyString a = new MyString(new char[] {'H', 101, 108, 108, 111, '!'});
+        String expectedResult = "Hello!";
+        String actualResult = a.toString();
+        Assertions.assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void lengthShouldReturnZeroWhenFieldSmbHaveNotSymbols() {
+        MyString a = new MyString(new char[]{});
+        int expectedResult = 0;
+        int actualResult = a.length();
+        Assertions.assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void lengthShouldReturnNullWhenFieldSmbIsNull() {
+        MyString a = new MyString(null);
+        int expectedResult = 0;
+        int actualResult = a.length();
+        Assertions.assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void charArtShouldReturnResultWhenIndexIsFirst() {
+        MyString a = new MyString(new char[]{'f', '2', '!', 'g'});
+        int index = 0;
+        char expectedResult ='f';
+        char actualResult = a.charArt(index);
+        Assertions.assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void charArtShouldReturnResultWhenIndexIsLast() {
+        MyString a = new MyString(new char[]{'f', '2', '!', 'g', 't', 'H'});
+        int index = 5;
+        char expectedResult ='H';
+        char actualResult = a.charArt(index);
+        Assertions.assertEquals(expectedResult, actualResult);
+    }
+}
