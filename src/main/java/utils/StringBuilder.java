7763526f7700fce6593958979b5ce19292cecde9
alexalexup <upvector.by@gmail.com>
2022-02-06 18:01:34 +0300

    add StringBuilderClass

---
+++ src/main/java/utils/StringBuilder.java
@@ -0,0 +1,70 @@
+package utils;
+
+public class StringBuilder {
+    private char[] data;
+    private int size;
+
+    public StringBuilder(int capacity ){
+        this.data = new char[capacity];
+    }
+
+    public StringBuilder() {
+        this.data = new char[16];
+    }
+
+    public int length() {
+        return this.size;
+    }
+
+    private void add(char symbol) {
+        if (this.size >= this.data.length) {
+            char[] newString = new char[this.length() * 2];
+            System.arraycopy(this.data, 0, newString, 0, this.size );
+            this.data = newString;
+        }
+        this.size++;
+        this.data[size - 1] = symbol;
+    }
+
+    public StringBuilder append(String s) {
+        for (int i = 0; i < s.length(); i++) {
+            this.add(s.toCharArray()[i]);
+        }
+        return this;
+    }
+
+    public StringBuilder append(boolean x){
+        String s = "" + x;
+        return this.append(s);
+    }
+
+    public StringBuilder append(int x){
+        String s = "" + x;
+        return this.append(s);
+    }
+
+    public StringBuilder append(double x){
+        String s = "" + x;
+        return this.append(s);
+    }
+
+    public StringBuilder append(long x){
+        String s = "" + x;
+        return this.append(s);
+    }
+
+    public String toString() {
+        if (this.size == 0) {
+            String s = "";
+            return s;
+        }
+        if (this.data.length == this.size) {
+            String s = new String(this.data);
+            return s;
+        }
+        char[] String = new char[size];
+        System.arraycopy(this.data,0,String,0,this.size);
+        String s = new String(String);
+        return s;
+    }
+}
---
+++ src/test/java/utils/StringBuilderTest.java
@@ -0,0 +1,44 @@
+package utils;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StringBuilderTest {
+
+    @Test
+    public void appendShouldAddDataToCurrentObjectWhenSizeIsZero() {
+        StringBuilder s = new StringBuilder();
+        String data = "Have a nice day";
+        String actualString = s.append(data).toString();
+        boolean actualResult = actualString.equals(data);
+        Assertions.assertTrue(actualResult);
+    }
+
+    @Test
+    public void appendShouldAddSomeTypesOfDataToCurrentObjectWhenMethodWasCalled() {
+        StringBuilder s = new StringBuilder();
+        String firstData = "One";
+        int secondData = 2;
+        double thirdData = 3.15;
+        boolean fourthData  = false;
+        String actualString = s.append(firstData)
+                .append(secondData)
+                .append(thirdData)
+                .append(fourthData)
+                .toString();
+        String expectedString = "One23.15false";
+        boolean actualResult = actualString.equals(expectedString);
+        Assertions.assertTrue(actualResult);
+    }
+
+    @Test
+    public void toStringShouldReturnResultWhenObjectHaveNotData() {
+        StringBuilder s = new StringBuilder();
+        String actualString = s.toString();
+        String expectedString = "";
+        boolean actualResult = actualString.equals(expectedString);
+        Assertions.assertTrue(actualResult);
+    }
+}
