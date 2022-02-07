2954d06168d1431f93e170e15f6d549174bf0255
alexalexup <upvector.by@gmail.com>
2022-02-07 22:54:15 +0300

    fix error3

--- src/main/java/benchmarks/StringBuilderBenchmark.java
+++ src/main/java/benchmarks/StringBuilderBenchmark.java
@@ -4,7 +4,7 @@ package benchmarks;
 public class StringBuilderBenchmark {
 
     public static String concatenateUsingOperator(int n) {
-        String result = " ";
+        String result = "";
         for (int i = 0; i < n; i++) {
             result += " " + i % 10;
         }
@@ -12,15 +12,15 @@ public class StringBuilderBenchmark {
     }
 
     public static String concatenateUsingStringBuilder(int n) {
-        StringBuilder result = new StringBuilder().append(" ");
+        StringBuilder result = new StringBuilder().append("");
         for (int i = 0; i < n; i++) {
             result.append(" " + i % 10);
         }
         return result.toString();
     }
 
-    //concatenateUsingOperator takes 39,06 sec
-    //concatenateUsingStringBuilder takes 0,07 sec
+    //concatenateUsingOperator takes 38,72 sec
+    //concatenateUsingStringBuilder takes 0,05 sec
     public static void main(String[] args) {
         double firstTime = System.currentTimeMillis();
         concatenateUsingOperator(500000);
--- src/test/java/benchmarks/StringBuilderBenchmarkTest.java
+++ src/test/java/benchmarks/StringBuilderBenchmarkTest.java
@@ -9,29 +9,43 @@ class StringBuilderBenchmarkTest {
 
     @Test
     public void concatenateUsingOperatorShouldReturnResultWhenArgumentIsZero() {
-        String expectedResult = " ";
+        String expectedResult = "";
         String actualResult = StringBuilderBenchmark.concatenateUsingOperator(0);
         Assertions.assertEquals(expectedResult,actualResult);
     }
 
     @Test
     public void concatenateUsingOperatorShouldReturnResultWhenMethodWasCalled() {
-        String expectedResult = "  0 1 2 3";
+        String expectedResult = " 0 1 2 3";
         String actualResult = StringBuilderBenchmark.concatenateUsingOperator(4);
         Assertions.assertEquals(expectedResult,actualResult);
     }
 
     @Test
+    public void concatenateUsingOperatorShouldReturnResultWhenArgumentIsTwenty() {
+        String expectedResult = " 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9";
+        String actualResult = StringBuilderBenchmark.concatenateUsingOperator(20);
+        Assertions.assertEquals(expectedResult,actualResult);
+    }
+
+    @Test
     public void concatenateUsingStringBuilderShouldReturnResultWhenArgumentIsZero() {
-        String expectedResult = " ";
+        String expectedResult = "";
         String actualResult = StringBuilderBenchmark.concatenateUsingStringBuilder(0);
         Assertions.assertEquals(expectedResult,actualResult);
     }
 
     @Test
     public void concatenateUsingStringBuilderShouldReturnResultWhenMethodWasCalled() {
-        String expectedResult = "  0 1 2 3";
+        String expectedResult = " 0 1 2 3";
         String actualResult = StringBuilderBenchmark.concatenateUsingStringBuilder(4);
+        Assertions.assertEquals(expectedResult,actualResult);
+    }
+
+    @Test
+    public void concatenateUsingStringBuilderShouldReturnResultWhenArgumentIsTwenty() {
+        String expectedResult = " 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9";
+        String actualResult = StringBuilderBenchmark.concatenateUsingStringBuilder(20);
         Assertions.assertEquals(expectedResult,actualResult);
     }
 }
