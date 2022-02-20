2224da465bf8542701c9bda819c02d5009d9f4db
alexalexup <upvector.by@gmail.com>
2022-02-20 17:59:26 +0300

    create sortBenchmark class, add Tests

--- src/main/java/benchmarks/SortBenchmark.java
+++ src/main/java/benchmarks/SortBenchmark.java
@@ -1,9 +1,20 @@
 package benchmarks;
 
 import java.util.Random;
+import entities.Event;
+import utils.ArrayUtils;
 
 public class SortBenchmark {
 
+    /**
+     * Create array with random length and with random numbers
+     * @cpu O(n), n - array.length
+     * @ram O(n), n - array.length
+     * @param n argument
+     * @param min argument
+     * @param max argument
+     * return array with random length and numbers
+     */
     public static int[] randomArray(int n, int min, int max) {
         Random random  = new Random(3);
         int delta = max - min;
@@ -13,5 +24,47 @@ public class SortBenchmark {
             array[i] = random.nextInt(delta) + min;
         }
         return array;
+    }
+
+    /**
+     * Create array with events. Method generate array with random length and data
+     * @cpu O(n), n - array.length
+     * @ram O(n), n - array.length
+     * @param n argument
+     * @param min argument
+     * @param max argument
+     * return array with events.
+     */
+    public static Event[] randomEvents(int n, int min, int max){
+        Random random = new Random(6);
+        int delta = max - min;
+        int length = random.nextInt(n);
+        Event[] events = new Event[length];
+        for (int i = 0; i < events.length; i ++) {
+            int year = random.nextInt(delta) + min;
+            int month = random.nextInt(12) + 1;
+            int day = random.nextInt(31) + 1;
+            String name = new StringBuilder(20).append(random.nextLong()).toString();
+            events[i] = new Event(year, month, day, name);
+        }
+        return events;
+    }
+
+    // bsp1: 0.04s
+    // bsp2: 0.03S
+    // bsp3: 2.33s
+    // bsp4: 2.31s
+    // bsp01: 0.26s
+    // bsp02: 0.19s
+    // bsp03: 27.76s
+    // bsp04: 25.12s
+    public static void main (String[] args) {
+        int[] numbersArray = randomArray(10000, -10000, 10000);
+        Event[] events = randomEvents(10000, 2000, 2021);
+        long firstTime = System.currentTimeMillis();
+        ArrayUtils.bubbleSort(numbersArray);
+        ArrayUtils.bubbleSort(events);
+        long secondTime = System.currentTimeMillis();
+        System.out.println(secondTime-firstTime + " ms");
     }
 }
--- src/test/java/benchmarks/SortBenchmarkTest.java
+++ src/test/java/benchmarks/SortBenchmarkTest.java
@@ -1,20 +1,62 @@
 package benchmarks;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import entities.Event;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class SortBenchmarkTest {
 
     @Test
-    public void sdfsdf() {
-        int[] a = SortBenchmark.randomArray(5, 2, 135);
-        for (int i = 0; i < a.length; i++) {
-            if ( a[i] == 135 || a[i] == 2) {
-                System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-            }
-            System.out.println(a[i] + " ");
+    public void randomArrayShouldReturnArraysWithEqualDataWhenArgumentsAreSame() {
+        int[] firstArray = SortBenchmark.randomArray(24, 2, 28);
+        int[] secondArray = SortBenchmark.randomArray(24, 2, 28);
+        Assertions.assertArrayEquals(firstArray,secondArray);
+    }
+
+    @Test
+    public void randomArrayShouldReturnResultWhenDataIncludeValueBetweenInclusiveMinArgumentAndExclusiveMaxArgument() {
+        int[] randomArray = SortBenchmark.randomArray(100,5, 124);
+        for (int i = 0; i < randomArray.length; i++) {
+            Assertions.assertFalse(randomArray[i] < 5 || randomArray[i] >= 124);
         }
     }
 
+    @Test
+    public void randomEventsShouldReturnArraysWithEqualDataWhenArgumentsAreSame() {
+        Event[] firstEvents = SortBenchmark.randomEvents(32, 1920, 1954);
+        Event[] secondEvents = SortBenchmark.randomEvents(32, 1920, 1954);
+        Assertions.assertEquals(firstEvents.length, secondEvents.length);
+        for (int i = 0; i < firstEvents.length; i++) {
+            Assertions.assertTrue(firstEvents[i].equals(secondEvents[i]));
+        }
+    }
+
+    @Test
+    public void randomEventsShouldReturnResultWhenYearIncludeValueBetweenInclusiveMinArgumentAndExclusiveMaxArgument() {
+        Event[] firstEvents = SortBenchmark.randomEvents(32, 1920, 1954);
+        for (int i = 0; i < firstEvents.length; i++) {
+            int year = firstEvents[i].getYear();
+            Assertions.assertFalse(year < 1920 || year >= 1954);
+        }
+    }
+
+    @Test
+    public void randomEventsShouldReturnResultWhenMonthIsInclusiveBetweenOneAndTwelve() {
+        Event[] firstEvents = SortBenchmark.randomEvents(32, 1920, 1954);
+        for (int i = 0; i < firstEvents.length; i++) {
+            int month = firstEvents[i].getMonth();
+            Assertions.assertTrue(month >= 1 || month <= 12);
+        }
+    }
+
+    @Test
+    public void randomEventsShouldReturnResultWhenDayIsInclusiveBetweenOneAndThirtyOne() {
+        Event[] firstEvents = SortBenchmark.randomEvents(32, 1920, 1954);
+        for (int i = 0; i < firstEvents.length; i++) {
+            int month = firstEvents[i].getMonth();
+            Assertions.assertTrue(month >= 1 || month <= 31);
+        }
+    }
 }
