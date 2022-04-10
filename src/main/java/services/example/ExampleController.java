72dcd1dd3abfcfa0e2211917bbbdba221ea2d9a2
alexalexup <upvector.by@gmail.com>
2022-04-11 00:47:36 +0300

    add java docs

--- src/main/java/services/example/ExampleController.java
+++ src/main/java/services/example/ExampleController.java
@@ -11,36 +11,84 @@ public class ExampleController {
     private Event event;
 
     @GetMapping("/")
+    /**
+     * Return text message
+     * @cpu O(1)
+     * @ram O(1)
+     * @return text message "I'm your first web-service"
+     */
     public String firstPage() {
         return "I'm your first web-service";
     }
 
     @GetMapping("/second")
+    /**
+     * Return text message
+     * @cpu O(1)
+     * @ram O(1)
+     * @return text message "I'm the second binding"
+     */
     public String secondPage() {
         return "I'm the second binding";
     }
 
     @PostMapping("/second")
+    /**
+     * Return text message
+     * @cpu O(1)
+     * @ram O(1)
+     * @return text message "I'm post mapping"
+     */
     public String postMethod() {
         return "I'm post mapping";
     }
 
     @PutMapping("/second")
+    /**
+     * Return text message
+     * @cpu O(1)
+     * @ram O(1)
+     * @return text message "I'm put mapping"
+     */
     public String putMethod() {
         return "I'm put mapping";
     }
 
     @PatchMapping("/second")
+    /**
+     * Return text message
+     * @cpu O(1)
+     * @ram O(1)
+     * @return text message "I'm patch mapping"
+     */
     public String patchMethod() {
         return "I'm patch mapping";
     }
 
     @DeleteMapping("/second")
+    /**
+     * Return text message
+     * @cpu O(1)
+     * @ram O(1)
+     * @return text message "I'm delete mapping"
+     */
     public String deleteMethod() {
         return "I'm delete mapping";
     }
 
     @PostMapping("/extract/{pathVariable}/{second}")
+    /**
+     * Return information from http request by query, path and body
+     * @cpu O(1)
+     * @ram O(1)
+     * @param pathVariable path parameter
+     * @param secondVariable path parameter
+     * @param required query parameter
+     * @param optional query parameter
+     * @param defaultValue query parameter
+     * @param body body from http request
+     * @return text message with information from http request
+     */
     public String extract(@PathVariable String pathVariable,
                           @PathVariable(name = "second") int secondVariable,
                           @RequestParam String required,
@@ -55,6 +103,14 @@ public class ExampleController {
                 + "body = " + body + "\n";
     }
 
+    /**
+     * Return String with numbers in range by input data since "from" by "to"
+     * @cpu O(n), n = Math.abs(to - from)
+     * @ram O(n), n = Math.abs(to - from)
+     * @param from
+     * @param to
+     * @return String with numbers in range by input data since "from" by "to"
+     */
     private String range(int from, int to) {
         int length = Math.abs(to - from);
         StringBuilder string = new StringBuilder(length);
@@ -66,21 +122,49 @@ public class ExampleController {
     }
 
     @GetMapping("api/range")
+    /**
+     * Return String with numbers in range by input data since "from" by "to"
+     * @cpu O(n), n = Math.abs(to - from)
+     * @ram O(n), n = Math.abs(to - from)
+     * @param from path parameter
+     * @param to path parameter
+     * @return String with numbers in range by input data since "from" by "to"
+     */
     public String numbFromParam(@RequestParam int from, @RequestParam int to) {
        return range(from, to);
     }
 
     @GetMapping("api/range/{param1}/{param2}")
+    /**
+     * Return String with numbers in range by input data since "from" by "to"
+     * @cpu O(n), n = Math.abs(to - from)
+     * @ram O(n), n = Math.abs(to - from)
+     * @param from path parameter
+     * @param to path parameter
+     * @return String with numbers in range by input data since "from" by "to"
+     */
     public String numbFromVariable(@PathVariable("param1") int from, @PathVariable("param2") int to) {
         return range(from, to);
     }
 
     @PostMapping("/api/event")
+    /**
+     * Get Event Object from body and save it
+     * @cpu O(1)
+     * @ram O(1)
+     * @param event body from http request
+     */
     public void postEvent(@RequestBody Event event) {
         this.event = event;
     }
 
     @GetMapping("/api/event")
+    /**
+     * Return Event Object
+     * @cpu O(1)
+     * @ram O(1)
+     * @return Event Objet
+     */
     public Event getEvent() {
         return this.event;
     }
--- src/main/java/services/queue/QueueController.java
+++ src/main/java/services/queue/QueueController.java
@@ -12,21 +12,44 @@ public class QueueController {
     private QueueManagementSystem queue = new QueueManagementSystem("bank");
 
     @GetMapping("/api/queue/nextTicket")
+    /**
+     * Create and return newTicket with number increased by one to the previous ticket
+     * @cpu O(1)
+     * @ram O(1)
+     * @return newTicket with number increased by one to the previous ticket
+     */
     public Ticket getNextTicket() {
         return this.queue.getNextTicket();
     }
 
     @GetMapping("/api/queue/totalTickets")
+    /**
+     * Get information about totalTickets from queue
+     * @cpu O(1)
+     * @ram O(1)
+     * @return information about totalTickets from queue
+     */
     public String getTotalTickets() {
         return "" + this.queue.getTotalTickets();
     }
 
    @PostMapping("api/queue/toNextWorkDay")
+   /**
+    * Switch queue to new work day. Increase by one size of ArrayList days and reset to zero value of totalTickets
+    * @cpu Θ(1)
+    * @ram O(n), n - days.size()
+    */
     public void  toNextWorkDay() {
         this.queue.toNextWorkDay();
    }
 
     @GetMapping("api/queue/getVisitsByDays")
+    /**
+     * Get information about tickets by days and return it in array
+     * @cpu O(n), n - days.size()
+     * @ram O(n), n - days.size()
+     * @return array with data about tickets by days
+     */
     public int[]  getVisitsByDays() {
         return this.queue.getVisitsByDay().toArray();
     }
