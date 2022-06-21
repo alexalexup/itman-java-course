d8aea34e459583d2969d0a862cb64a40e364aa72
alexalexup <upvector.by@gmail.com>
2022-06-22 02:40:43 +0300

    add JavaDocs and Tests 2

--- src/test/java/services/queue/QueueControllerTest.java
+++ src/test/java/services/queue/QueueControllerTest.java
@@ -15,52 +15,13 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 class QueueControllerTest {
 
     @Nested
-    public class MethodsFromQueueSpringApplication {
+    public class MethodsFromQueueSpringApplicationFirstScript {
 
         @Autowired
         private MockMvc mockMvc;
 
         @Test
         public void methodsFromQueueSpringApplicationShouldWorkCorrectWhenFirstScriptWasCalled() throws Exception {
-            MockHttpServletRequestBuilder totalRequest = MockMvcRequestBuilders
-                    .get("/api/queue/totalTickets");
-            this.mockMvc.perform(totalRequest)
-                    .andExpect(MockMvcResultMatchers.status().isOk())
-                    .andExpect(MockMvcResultMatchers.content().string("0"));
-            MockHttpServletRequestBuilder nextTicketRequest = MockMvcRequestBuilders
-                    .get("/api/queue/nextTicket");
-            this.mockMvc.perform(nextTicketRequest);
-            this.mockMvc.perform(nextTicketRequest);
-            this.mockMvc.perform(nextTicketRequest);
-            this.mockMvc.perform(nextTicketRequest);
-            this.mockMvc.perform(nextTicketRequest)
-                    .andExpect(MockMvcResultMatchers.status().isOk())
-                    .andExpect(MockMvcResultMatchers.content().json("{\"number\":5, \"place\":\"bank\"}"));
-            MockHttpServletRequestBuilder nextWorkDayRequest = MockMvcRequestBuilders
-                    .post("/api/queue/toNextWorkDay");
-            this.mockMvc.perform(nextWorkDayRequest);
-            this.mockMvc.perform(nextTicketRequest);
-            this.mockMvc.perform(nextTicketRequest);
-            this.mockMvc.perform(nextTicketRequest)
-                    .andExpect(MockMvcResultMatchers.status().isOk())
-                    .andExpect(MockMvcResultMatchers.content().json("{\"number\":3, \"place\":\"bank\"}"));
-            this.mockMvc.perform(nextWorkDayRequest);
-            this.mockMvc.perform(nextTicketRequest);
-            this.mockMvc.perform(nextTicketRequest);
-            this.mockMvc.perform(nextTicketRequest);
-            this.mockMvc.perform(nextTicketRequest);
-            MockHttpServletRequestBuilder getVisitsByDaysRequest = MockMvcRequestBuilders
-                    .get("/api/queue/getVisitsByDays");
-            this.mockMvc.perform(getVisitsByDaysRequest)
-                    .andExpect(MockMvcResultMatchers.status().isOk())
-                    .andExpect(MockMvcResultMatchers.content().json("[5,3,4]"));
-            this.mockMvc.perform(totalRequest)
-                    .andExpect(MockMvcResultMatchers.status().isOk())
-                    .andExpect(MockMvcResultMatchers.content().string("12"));
-        }
-
-        @Test
-        public void methodsFromQueueSpringApplicationShouldWorkCorrectWhenSecondScriptWasCalled() throws Exception {
             MockHttpServletRequestBuilder nextTicketRequest = MockMvcRequestBuilders
                     .get("/api/queue/nextTicket");
             this.mockMvc.perform(nextTicketRequest);
@@ -86,6 +47,37 @@ class QueueControllerTest {
             this.mockMvc.perform(getCurrentQueue).andExpect(MockMvcResultMatchers.status().isOk())
                     .andExpect(MockMvcResultMatchers.content().json("[{\"number\":1,\"place\":\"bank\"}" +
                             ",{\"number\":2,\"place\":\"bank\"}]"));
+            MockHttpServletRequestBuilder totalRequest = MockMvcRequestBuilders
+                    .get("/api/queue/totalTickets");
+            this.mockMvc.perform(totalRequest)
+                    .andExpect(MockMvcResultMatchers.status().isOk())
+                    .andExpect(MockMvcResultMatchers.content().string("5"));
+            this.mockMvc.perform(nextTicketRequest);
+            this.mockMvc.perform(nextTicketRequest);
+            this.mockMvc.perform(nextTicketRequest);
+            this.mockMvc.perform(nextTicketRequest);
+            this.mockMvc.perform(nextTicketRequest)
+                    .andExpect(MockMvcResultMatchers.status().isOk())
+                    .andExpect(MockMvcResultMatchers.content().json("{\"number\":7, \"place\":\"bank\"}"));
+            this.mockMvc.perform(nextWorkDayRequest);
+            this.mockMvc.perform(nextTicketRequest);
+            this.mockMvc.perform(nextTicketRequest);
+            this.mockMvc.perform(nextTicketRequest)
+                    .andExpect(MockMvcResultMatchers.status().isOk())
+                    .andExpect(MockMvcResultMatchers.content().json("{\"number\":3, \"place\":\"bank\"}"));
+            this.mockMvc.perform(nextWorkDayRequest);
+            this.mockMvc.perform(nextTicketRequest);
+            this.mockMvc.perform(nextTicketRequest);
+            this.mockMvc.perform(nextTicketRequest);
+            this.mockMvc.perform(nextTicketRequest);
+            MockHttpServletRequestBuilder getVisitsByDaysRequest = MockMvcRequestBuilders
+                    .get("/api/queue/getVisitsByDays");
+            this.mockMvc.perform(getVisitsByDaysRequest)
+                    .andExpect(MockMvcResultMatchers.status().isOk())
+                    .andExpect(MockMvcResultMatchers.content().json("[3,7,3,4]"));
+            this.mockMvc.perform(totalRequest)
+                    .andExpect(MockMvcResultMatchers.status().isOk())
+                    .andExpect(MockMvcResultMatchers.content().string("17"));
         }
     }
 }
