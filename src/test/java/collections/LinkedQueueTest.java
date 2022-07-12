e8d76070ddbd8eeb06a68d7e42742803d644286b
alexalexup <upvector.by@gmail.com>
2022-07-12 23:36:20 +0300

    add Queue interface

--- src/test/java/collections/LinkedQueueTest.java
+++ src/test/java/collections/LinkedQueueTest.java
@@ -4,26 +4,26 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
-class QueueTest {
+class LinkedQueueTest {
 
     @Nested
     public class Offer {
         @Test
         public void shouldAddElementInTheEndOfTheQueue() {
-            Queue queue = new Queue();
+            LinkedQueue linkedQueue = new LinkedQueue();
             for (int i = 1; i < 11; i++) {
-                queue.offer(i);
+                linkedQueue.offer(i);
             }
-            Object[] actualResult = queue.toArray();
+            Object[] actualResult = linkedQueue.toArray();
             Object[] expectedResult = new Object[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
             Assertions.assertArrayEquals(expectedResult, actualResult);
         }
 
         @Test
         public void shouldAddElementWhenQueueHaveNotElements() {
-            Queue queue = new Queue();
-            queue.offer(1);
-            Object[] actualResult = queue.toArray();
+            LinkedQueue linkedQueue = new LinkedQueue();
+            linkedQueue.offer(1);
+            Object[] actualResult = linkedQueue.toArray();
             Object[] expectedResult = new Object[] {1};
             Assertions.assertArrayEquals(expectedResult, actualResult);
         }
@@ -33,19 +33,19 @@ class QueueTest {
     public class Peek {
         @Test
         public void shouldReturnFirsElementFromQueueWhenQueueHaveSomeElements() {
-            Queue queue = new Queue();
-            queue.offer(1);
-            queue.offer(2);
-            queue.offer(3);
-            Object actualResult = queue.peek();
+            LinkedQueue linkedQueue = new LinkedQueue();
+            linkedQueue.offer(1);
+            linkedQueue.offer(2);
+            linkedQueue.offer(3);
+            Object actualResult = linkedQueue.peek();
             Assertions.assertEquals(1, actualResult);
         }
 
         @Test
         public void shouldReturnFirsElementFromQueueWhenQueueHaveOneElement() {
-            Queue queue = new Queue();
-            queue.offer(5);
-            Object actualResult = queue.peek();
+            LinkedQueue linkedQueue = new LinkedQueue();
+            linkedQueue.offer(5);
+            Object actualResult = linkedQueue.peek();
             Assertions.assertEquals(5, actualResult);
         }
     }
@@ -54,22 +54,22 @@ class QueueTest {
     public class Poll {
         @Test
         public void shouldReturnAndDeleteFirsElementFromQueueWhenQueueHaveSomeElements() {
-            Queue queue = new Queue();
-            queue.offer(1);
-            queue.offer(2);
-            queue.offer(3);
-            Object actualResult = queue.poll();
-            Object[] actualArray = queue.toArray();
+            LinkedQueue linkedQueue = new LinkedQueue();
+            linkedQueue.offer(1);
+            linkedQueue.offer(2);
+            linkedQueue.offer(3);
+            Object actualResult = linkedQueue.poll();
+            Object[] actualArray = linkedQueue.toArray();
             Assertions.assertEquals(1, actualResult);
             Assertions.assertArrayEquals(new Object[]{2, 3}, actualArray);
         }
 
         @Test
         public void shouldReturnAndDeleteFirsElementFromQueueWhenQueueHaveOneElement() {
-            Queue queue = new Queue();
-            queue.offer(1);
-            Object actualResult = queue.poll();
-            Object[] actualArray = queue.toArray();
+            LinkedQueue linkedQueue = new LinkedQueue();
+            linkedQueue.offer(1);
+            Object actualResult = linkedQueue.poll();
+            Object[] actualArray = linkedQueue.toArray();
             Assertions.assertEquals(1, actualResult);
             Assertions.assertArrayEquals(new Object[]{}, actualArray);
         }
@@ -79,26 +79,26 @@ class QueueTest {
     public class Size {
         @Test
         public void shouldReturnZeroWhenQueueHaveNotAnyElements() {
-            Queue queue = new Queue();
-            int actualResult = queue.size();
+            LinkedQueue linkedQueue = new LinkedQueue();
+            int actualResult = linkedQueue.size();
             Assertions.assertEquals(0,actualResult);
         }
 
         @Test
         public void shouldReturnOneWhenQueueHaveOneElement() {
-            Queue queue = new Queue();
-            queue.offer(1);
-            int actualResult = queue.size();
+            LinkedQueue linkedQueue = new LinkedQueue();
+            linkedQueue.offer(1);
+            int actualResult = linkedQueue.size();
             Assertions.assertEquals(1,actualResult);
         }
 
         @Test
         public void shouldReturnResultWhenQueueHaveSomeElements() {
-            Queue queue = new Queue();
+            LinkedQueue linkedQueue = new LinkedQueue();
             for (int i = 0; i < 24; i++) {
-                queue.offer(i);
+                linkedQueue.offer(i);
             }
-            int actualResult = queue.size();
+            int actualResult = linkedQueue.size();
             Assertions.assertEquals(24,actualResult);
         }
     }
@@ -107,24 +107,24 @@ class QueueTest {
     public class IsEmpty {
         @Test
         public void shouldReturnFalseWhenQueueHaveOneElement() {
-            Queue queue = new Queue();
-            queue.offer(1);
-            Assertions.assertFalse(queue.isEmpty());
+            LinkedQueue linkedQueue = new LinkedQueue();
+            linkedQueue.offer(1);
+            Assertions.assertFalse(linkedQueue.isEmpty());
         }
 
         @Test
         public void shouldReturnFalseWhenQueueHaveSomeElements() {
-            Queue queue = new Queue();
-            queue.offer(1);
-            queue.offer(2);
-            queue.offer(3);
-            Assertions.assertFalse(queue.isEmpty());
+            LinkedQueue linkedQueue = new LinkedQueue();
+            linkedQueue.offer(1);
+            linkedQueue.offer(2);
+            linkedQueue.offer(3);
+            Assertions.assertFalse(linkedQueue.isEmpty());
         }
 
         @Test
         public void shouldReturnTrueWhenQueueHaveNotAnyElements() {
-            Queue queue = new Queue();
-            Assertions.assertTrue(queue.isEmpty());
+            LinkedQueue linkedQueue = new LinkedQueue();
+            Assertions.assertTrue(linkedQueue.isEmpty());
         }
     }
 
@@ -132,18 +132,18 @@ class QueueTest {
     public class ToArray {
         @Test
         public void shouldReturnArrayWithoutElementsWhenQueueHaveNotAnyElements() {
-            Queue queue = new Queue();
-            Object[] actualResult = queue.toArray();
+            LinkedQueue linkedQueue = new LinkedQueue();
+            Object[] actualResult = linkedQueue.toArray();
             Assertions.assertArrayEquals(new Object[]{}, actualResult);
         }
 
         @Test
         public void shouldReturnArrayWhenQueueHaveSomeElements() {
-            Queue queue = new Queue();
-            queue.offer(1);
-            queue.offer(2);
-            queue.offer(3);
-            Object[] actualResult = queue.toArray();
+            LinkedQueue linkedQueue = new LinkedQueue();
+            linkedQueue.offer(1);
+            linkedQueue.offer(2);
+            linkedQueue.offer(3);
+            Object[] actualResult = linkedQueue.toArray();
             Assertions.assertArrayEquals(new Object[]{1, 2, 3}, actualResult);
         }
     }
