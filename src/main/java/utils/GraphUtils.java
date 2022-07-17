77cc683db99def819e9857a6dd2aae2a16f64ae7
alexalexup <upvector.by@gmail.com>
2022-07-17 22:32:50 +0300

    add toUndirectedAdjacencyList method

--- src/main/java/utils/GraphUtils.java
+++ src/main/java/utils/GraphUtils.java
@@ -14,4 +14,16 @@ public class GraphUtils {
         }
         return list;
     }
+
+    public static IntArrayList[] toUndirectedAdjacencyList(int vertices, Pair[] edges) {
+        IntArrayList[] list = new IntArrayList[vertices + 1];
+        for (int i = 0; i < list.length; i++) {
+            list[i] = new IntArrayList();
+        }
+        for (int i = 0; i < edges.length; i++) {
+            list[edges[i].getLeft()].add(edges[i].getRight());
+            list[edges[i].getRight()].add(edges[i].getLeft());
+        }
+        return list;
+    }
 }
