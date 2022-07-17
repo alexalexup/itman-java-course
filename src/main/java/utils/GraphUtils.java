f0a0c43c986d6dc86b9dda1a4d082561b6f0d986
alexalexup <upvector.by@gmail.com>
2022-07-17 22:07:16 +0300

    add toAdjacencyList method

---
+++ src/main/java/utils/GraphUtils.java
@@ -0,0 +1,17 @@
+package utils;
+
+import entities.Pair;
+import collections.IntArrayList;
+
+public class GraphUtils {
+    public static IntArrayList[] toAdjacencyList(int vertices, Pair[] edges) {
+        IntArrayList[] list = new IntArrayList[vertices + 1];
+        for (int i = 0; i < list.length; i++) {
+            list[i] = new IntArrayList();
+        }
+        for (int i = 0; i < edges.length; i++) {
+            list[edges[i].getLeft()].add(edges[i].getRight());
+        }
+        return list;
+    }
+}
