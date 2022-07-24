90c0b69828db4490a216c7234e78151685dc2a03
alexalexup <upvector.by@gmail.com>
2022-07-24 22:25:34 +0300

    add some edits

--- src/main/java/services/calculation/CalculationController.java
+++ src/main/java/services/calculation/CalculationController.java
@@ -8,6 +8,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class CalculationController {
 
 
+    /**
+     * Do math operation on the numbers that method gets
+     * @cpu O(1)
+     * @ram O(1)
+     * @param term1 argument
+     * @param operator math operation that method  do
+     * @param term2 argument
+     * @return result of the math operation on the numbers that user passed
+     */
     @GetMapping("/calculation")
     public double getCalculation(@RequestParam double term1, @RequestParam String operator, @RequestParam double term2) {
         Math math = Math.valueOf(operator);
@@ -15,6 +24,12 @@ public class CalculationController {
         return math.Calculate();
     }
 
+    /**
+     * Return all constans from Math enum (all operation that we can do)
+     * @cpu O(n), n - Math.values().length
+     * @ram O(n), n - Math.values().length
+     * @return result of the math operation on the numbers that user passed
+     */
     @GetMapping("/calculation/operators")
     public Object[] getOperators() {
         return Math.values();
