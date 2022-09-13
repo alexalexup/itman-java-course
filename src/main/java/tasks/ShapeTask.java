6a54a745ca4b8eab999bbec8062cd877f2fc48f5
alexalexup <upvector.by@gmail.com>
2022-09-14 00:11:33 +0300

    task 652 solved

--- src/main/java/tasks/ShapeTask.java
+++ src/main/java/tasks/ShapeTask.java
@@ -1,10 +1,24 @@
 package tasks;
+
 import collections.LinkedList;
 import entities.Shape;
 
+public class ShapeTask {
+    /**
+     * Returns the average value of parameters from the list shapes
+     * @cpu O(n), n - list.size()
+     * @ram O(1)
+     * @param list LinkedList with shapes
+     * @return the average value of parameters from the list shapes
+     */
+    public static double calcAvrPerimeter(LinkedList list) {
+        double sum = 0;
+        for (int i = 0; i < list.size(); i++) {
+            sum = sum + ((Shape) list.get(i)).calcPerimeter();
+        }
+        return sum / list.size();
+    }
 
-
-public class MinSquare {
     /**
      * Find shape with minimum square from the list
      * @cpu O(n), n - list.size()
--- src/test/java/tasks/ShapeTaskTest.java
+++ src/test/java/tasks/ShapeTaskTest.java
@@ -1,16 +1,44 @@
 package tasks;
 
-
 import collections.LinkedList;
+import collections.List;
 import entities.Circle;
 import entities.Rectangle;
+import entities.Shape;
 import entities.Square;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+class ShapeTaskTest {
+    private double sumPerimeter(List list) {
+        double sum = 0;
+        for (int i = 0; i < list.size(); i++) {
+            sum = sum + ((Shape) list.get(i)).calcPerimeter();
+        }
+        return sum;
+    }
+    @Nested
+    public class AvrPerimeter {
+        @Test
+        public void shouldReturnAveragePerimeterWhenShapesAreRectangles() {
+            LinkedList list = new LinkedList();
+            list.addFirst(new Rectangle(3, 2));
+            list.addFirst(new Rectangle(2, 8));
+            list.addFirst(new Rectangle(3.24, 22.1));
+            list.addFirst(new Rectangle(3, 2));
+            Assertions.assertEquals(sumPerimeter(list) / 4, ShapeTask.calcAvrPerimeter(list));
+        }
 
-class MinSquareTest {
+        @Test
+        public void shouldReturnAveragePerimeterWhenListHaveDifferentShapes() {
+            LinkedList list = new LinkedList();
+            list.addFirst(new Circle(3.22));
+            list.addFirst(new Square(5.66));
+            list.addFirst(new Rectangle(44.5, 28.9));
+            Assertions.assertEquals(sumPerimeter(list) /  3, ShapeTask.calcAvrPerimeter(list));
+        }
+    }
 
     @Nested
     public class FindMinSquareShape {
@@ -21,7 +49,7 @@ class MinSquareTest {
             list.addFirst(new Square(4.22));
             list.addFirst(new Square(5.66));
             list.addFirst(new Rectangle(6.44, 2.43));
-            Object result = MinSquare.findMinSquareShape(list);
+            Object result = ShapeTask.findMinSquareShape(list);
             Assertions.assertSame(result, list.get(0));
         }
 
@@ -34,8 +62,8 @@ class MinSquareTest {
             list.addFirst(new Rectangle(6.44, 2.43));
             list.addFirst(new Square(5.66));
             list.addFirst(new Rectangle(6.44, 2.43));
-            Object result = MinSquare.findMinSquareShape(list);
-           Assertions.assertEquals(list.get(0), result);
+            Object result = ShapeTask.findMinSquareShape(list);
+            Assertions.assertEquals(list.get(0), result);
         }
 
         @Test
@@ -47,7 +75,7 @@ class MinSquareTest {
             list.addFirst(new Square(2.43));
             list.addFirst(new Square(5.66));
             list.addFirst(new Square(6.44));
-            Object result = MinSquare.findMinSquareShape(list);
+            Object result = ShapeTask.findMinSquareShape(list);
             Assertions.assertEquals(list.get(2) , result);
         }
     }
