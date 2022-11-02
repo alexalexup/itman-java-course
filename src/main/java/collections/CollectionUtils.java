907065b170ce6cb0d94df404d4efffd6cb5bf80a
alexalexup <upvector.by@gmail.com>
2022-11-02 22:59:54 +0300

    some edits

--- src/main/java/collections/CollectionUtils.java
+++ src/main/java/collections/CollectionUtils.java
@@ -2,28 +2,71 @@ package collections;
 
 import java.util.Comparator;
 
+
 public class CollectionUtils {
 
-    public static <T> T findMin(List<T> list, Comparator comparator){
-        int min = 0;
-        for (int i = 0; i < list.size(); i++) {
-            if (comparator.compare(list.get(min), list.get(i)) > 0) {
+    /**
+     * Finds the minimum element in the List
+     * @cpu O(n), list.size()
+     * @ram O(1)
+     * @param list List with elements
+     * @param comparator argument
+     * @return minimum element from the List
+     */
+    public static <T> T findMin(List<T> list, Comparator<? super T> comparator){
+        int index = 0;
+        while (list.get(index) == null && index < list.size()) {
+            index++;
+        }
+        int min = index;
+        if (min == list.size()) {
+            return null;
+        }
+        if (min == list.size() - 1) {
+            return list.get(min);
+        }
+        for (int i = index; i < list.size(); i++) {
+            if (list.get(i) != null && comparator.compare(list.get(min), list.get(i)) > 0) {
                 min = i;
             }
         }
         return list.get(min);
     }
 
-    public static <T> T findMax(List<T> list, Comparator comparator) {
-        int max = 0;
-        for (int i = 0; i < list.size(); i++) {
-            if (comparator.compare(list.get(max), list.get(i)) < 0) {
+    /**
+     * Finds the maximum element in the List
+     * @cpu O(n), list.size()
+     * @ram O(1)
+     * @param list List with elements
+     * @param comparator argument
+     * @return minimum element from the List
+     */
+    public static <T> T findMax(List<T> list, Comparator<? super T> comparator) {
+        int index = 0;
+        while (list.get(index) == null && index < list.size()) {
+            index++;
+        }
+        int max = index;
+        if (max == list.size()) {
+            return null;
+        }
+        if (max == list.size() - 1) {
+            return list.get(max);
+        }
+        for (int i = index; i < list.size(); i++) {
+            if (list.get(i) != null && comparator.compare(list.get(max), list.get(i)) < 0) {
                 max = i;
             }
         }
         return list.get(max);
     }
 
+    /**
+     * Sort in increase order elements in the List
+     * @cpu O(n^2), list.size()
+     * @ram O(1)
+     * @param list List with elements
+     */
     public static <T> void sort(List<T> list) {
         if (list == null || list.size() == 1) {
             return;
@@ -41,6 +84,12 @@ public class CollectionUtils {
         }
     }
 
+    /**
+     * Reverse order of elements in the list
+     * @cpu O(n), list.size()
+     * @ram O(1)
+     * @param list List with elements
+     */
     public static <T> void reverse(List<T> list) {
         if (list == null || list.size() == 1) {
             return;
---
+++ src/test/java/collections/CollectionUtilsTest.java
@@ -0,0 +1,182 @@
+package collections;
+
+import entities.Event;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import java.util.Comparator;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CollectionUtilsTest {
+
+    @Nested
+    public class FindMin {
+        @Test
+        public void shouldFindMinElementInTheListWhenListHaveSomeElements() {
+             Lists<Integer> lists = new ArrayList<>();
+             lists.add(4);
+             lists.add(2);
+             lists.add(-3);
+             lists.add(10);
+             lists.add(4);
+             lists.add(2);
+             lists.add(0);
+             Comparator<Integer> comparator = (a, b) -> a.compareTo(b);
+             Integer result =  CollectionUtils.findMin(lists, comparator);
+             Assertions.assertEquals(result,-3);
+        }
+
+        @Test
+        public void shouldFindMinElementInTheListWhenListHaveOneElement() {
+            Lists<Double> lists = new ArrayList<>();
+            lists.add(4.22);
+            Comparator<Double> comparator = (a, b) -> a.compareTo(b);
+            Double result =  CollectionUtils.findMin(lists, comparator);
+            Assertions.assertEquals(result,4.22);
+        }
+
+        @Test
+        public void shouldFindMinElementInTheListWhenListHaveNullElements() {
+            Lists<Integer> lists = new LinkedList<>();
+            lists.add(null);
+            lists.add(10);
+            lists.add(4);
+            lists.add(2);
+            lists.add(null);
+            Comparator<Integer> comparator = (a, b) -> a.compareTo(b);
+            Integer result =  CollectionUtils.findMin(lists, comparator);
+            Assertions.assertEquals(result,2);
+        }
+
+        @Test
+        public void shouldWorkWhenListHaveOnlyNullElements() {
+            Lists<Integer> lists = new ArrayList<>();
+            lists.add(null);
+            lists.add(null);
+            lists.add(null);
+            lists.add(null);
+            lists.add(null);
+            Comparator<Integer> comparator = (a, b) -> a.compareTo(b);
+            Integer result =  CollectionUtils.findMin(lists, comparator);
+            Assertions.assertEquals(result,null);
+        }
+    }
+
+    @Nested
+    public class FindMax {
+        @Test
+        public void shouldFindMaxElementInTheListWhenListHaveSomeElements() {
+            Lists<Integer> lists = new LinkedList<>();
+            lists.add(4);
+            lists.add(2);
+            lists.add(-3);
+            lists.add(10);
+            lists.add(4);
+            lists.add(2);
+            lists.add(0);
+            Comparator<Integer> comparator = (a, b) -> a.compareTo(b);
+            Integer result =  CollectionUtils.findMax(lists, comparator);
+            Assertions.assertEquals(result,10);
+        }
+
+        @Test
+        public void shouldFindMaxElementInTheListWhenListHaveOneElement() {
+            Lists<Double> lists = new ArrayList<>();
+            lists.add(2.22);
+            Comparator<Double> comparator = (a, b) -> a.compareTo(b);
+            Double result =  CollectionUtils.findMax(lists, comparator);
+            Assertions.assertEquals(result,2.22);
+        }
+
+        @Test
+        public void shouldFindMaxElementInTheListWhenListHaveNullElements() {
+            Lists<Integer> lists = new LinkedList<>();
+            lists.add(null);
+            lists.add(10);
+            lists.add(4);
+            lists.add(2);
+            lists.add(null);
+            Comparator<Integer> comparator = (a, b) -> a.compareTo(b);
+            Integer result =  CollectionUtils.findMax(lists, comparator);
+            //Assertions.assertEquals(result,10);
+        }
+
+        @Test
+        public void shouldWorkWhenListHaveOnlyNullElements() {
+            Lists<Integer> lists = new ArrayList<>();
+            lists.add(null);
+            lists.add(null);
+            lists.add(null);
+            lists.add(null);
+            lists.add(null);
+            Comparator<Integer> comparator = (a, b) -> a.compareTo(b);
+            Integer result =  CollectionUtils.findMax(lists, comparator);
+            Assertions.assertEquals(result,null);
+        }
+    }
+
+    @Nested
+    public class Sort {
+        @Test
+        public void shouldSortElementWhenListHaveSomeElements() {
+            Lists<Integer> lists = LinkedList.of(4, 2, -3, 10, 4, 2, 0);
+            CollectionUtils.sort(lists);
+            Lists<Integer>  expected = LinkedList.of(-3, 0, 2, 2, 4, 4, 10);
+            Assertions.assertTrue(lists.equals(expected));
+        }
+
+        @Test
+        public void shouldSortElementWhenListHaveOneElement() {
+            Lists<Integer> lists = LinkedList.of(5);
+            CollectionUtils.sort(lists);
+            Lists<Integer>  expected = LinkedList.of(5);
+            Assertions.assertTrue(lists.equals(expected));
+        }
+
+        @Test
+        public void shouldSortElementWhenListHaveSomeEqualsElements() {
+            Lists<Integer> lists = LinkedList.of(5, 4, 4, 2, 2, 1);
+            CollectionUtils.sort(lists);
+            Lists<Integer>  expected = LinkedList.of(1, 2, 2, 4, 4, 5);
+            Assertions.assertTrue(lists.equals(expected));
+        }
+
+        @Test
+        public void shouldWorkWhenListHaveNotAnyElements() {
+            Lists<Integer> lists = LinkedList.of();
+            CollectionUtils.sort(lists);
+            Lists<Integer>  expected = LinkedList.of();
+            Assertions.assertTrue(lists.equals(expected));
+        }
+    }
+
+    @Nested
+    public class Reverse {
+        @Test
+        public void shouldReverseListWhenListHaveSomeElements() {
+            Lists<Double> list =  ArrayList.of(3.12, 4.22, 5.66, 0);
+            CollectionUtils.reverse(list);
+            Lists<Double> expected = LinkedList.of(0, 5.66, 4.22, 3.12);
+            Assertions.assertEquals(expected, list);
+        }
+
+        @Test
+        public void shouldReverseListWhenListHaveDifferentObjects() {
+            Lists list = LinkedList.of("Hi", 3, 3.12, true, null, "Bye");
+            List expected = LinkedList.of("Bye", null, true, 3.12, 3, "Hi");
+            CollectionUtils.reverse(list);
+            Assertions.assertEquals(expected, list);
+        }
+
+        @Test
+        public void shouldReverseListWhenListHaveOneElement() {
+            Lists list = LinkedList.of("Hi");
+            List expected = LinkedList.of("Hi");
+            CollectionUtils.reverse(list);
+            Assertions.assertEquals(expected, list);
+        }
+    }
+
+}
