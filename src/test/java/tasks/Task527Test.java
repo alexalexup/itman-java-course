2dea1427f2f89996f0a24c6f392a6bea3840cecc
alexalexup <upvector.by@gmail.com>
2022-11-14 23:16:33 +0300

    task 527

--- src/test/java/tasks/Task527Test.java
+++ src/test/java/tasks/Task527Test.java
@@ -1,7 +1,19 @@
 package tasks;
 
-import collections.IntArrayList;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+class Task527Test {
+
+    @Nested
+    public class Solve {
+        @Test
+        public void shouldReturnLastElementFromStackWhenAnyElementsLeftIsTheStack() {
+            int[] books = new int[]{1, 2, 3, 4, 5};
+            int[] queue = new int[]{2, 4, 4, -1, -1, 3, -1, 2};
+            int actualResult = Task514.solve(books, queue);
+            Assertions.assertEquals(7,actualResult);
+        }
+    }
+}
