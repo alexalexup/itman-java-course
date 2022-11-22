b9cae26877b37de457cfa646cc36fd03a8ae5ac5
alexalexup <upvector.by@gmail.com>
2022-11-22 19:43:06 +0300

    edit

---
+++ src/main/java/collections/ListIterator.java
@@ -0,0 +1,24 @@
+package collections;
+
+import java.util.Iterator;
+
+public interface ListIterator<T> extends Iterator<T> {
+
+    /**
+     * Method uses for set element to the current position that was  called from ListIterator Class
+     * @param element argument
+     * @return element from list by index
+     */
+    public void set(T element);
+
+    /**
+     * Method uses for set element to the before current position that was  called from ListIterator Class
+     * @param element argument
+     * @return element from list by index
+     */
+    public void insertBefore(T element);
+
+    public int getIteratorSize();
+
+    public void decreaseIteratorSize();
+}
